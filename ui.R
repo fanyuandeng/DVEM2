aa0612beb2456728e330908feee9d3dca94ecb14
y41u42002 <38520188+y41u42002@users.noreply.github.com>
2018-04-19 15:33:54 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,16 @@
+# Define UI for application that draws a histogram
+dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
+)
