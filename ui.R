5ad1a37c70884ca6ecf6fcbf0b9db17e90b3db44
y41u42002 <38520188+y41u42002@users.noreply.github.com>
2018-07-19 13:40:25 +0800

    Add files via upload

--- ui.R
+++ ui.R
@@ -40,18 +40,24 @@ ui<-dashboardPage(
                 box(
                   title = strong('欢迎使用'),
                   width = 200,
-                  img(src="logo.png", height = 200, width = 200),
-                  p("p creates a paragraph of text."),
-                  p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
-                  strong("strong() makes bold text."),
-                  em("em() creates italicized (i.e, emphasized) text."),
-                  br(),
-                  code("code displays your text similar to computer code"),
-                  div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
-                  br(),
-                  p("span does the same thing as div, but it works with",
-                    span("groups of words", style = "color:blue"),
-                    "that appear inside a paragraph.")
+                  column(
+                    img(src="logo.png", height = 200, width = 200),
+                    width = 4
+                  ),
+                  column(
+                    p("p creates a paragraph of text."),
+                    p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+                    strong("strong() makes bold text."),
+                    em("em() creates italicized (i.e, emphasized) text."),
+                    br(),
+                    code("code displays your text similar to computer code"),
+                    div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+                    br(),
+                    p("span does the same thing as div, but it works with",
+                      span("groups of words", style = "color:blue"),
+                      "that appear inside a paragraph."),
+                    width = 8
+                  )
                 )
               )
       ),
