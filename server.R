8f5001af40e8d805ded1382a91af8f7c93dca590
y41u42002 <38520188+y41u42002@users.noreply.github.com>
2018-07-19 12:09:26 +0800

    Add files via upload

--- server.R
+++ server.R
@@ -1,10 +1,210 @@
-# Define server logic required to draw a histogram
-function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
+server<-shinyServer(function(input, output, session) {
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+  observe({
+    
+    val<-input$SOURCE
+    
+    if(is.null(val)) {
+      return('Null')
+    }
+    
+    val<-fread(val$datapath)
+    val<-val$Time %>% as.POSIXct('%Y-%m-%d %H:%M:%S', tz = 'UTC')
+    val<-c(min(val), max(val))
+    
+    updateSliderInput(session, "Trange", value = c(val[1],val[2]),
+                      min = val[1], max = val[2], step = 1)
+    
+    visdata<-reactive({
+      
+      rawpath<-input$SOURCE
+      
+      if(is.null(rawpath)) {
+        return('Null')
+      }
+      
+      TLmin<-input$Trange[1]
+      TLmax<-input$Trange[2]
+      
+      raw<-fread(rawpath$datapath)[,Time:=as.POSIXct(Time, '%Y-%m-%d %H:%M:%S', tz = 'UTC')][,Speed:=Speed/3.6]
+      raw<-raw[,Time:= Time - hours(8)]# Time adjust
+      raw<-raw[Time >= (TLmin - hours(8)) & Time <= (TLmax - hours(8))]# Time adjust
+      
+    })
+    
+    visplot<-reactive({
+      
+      visdata %>%
+        ggvis(x = ~Time, y = ~Speed) %>%
+        layer_points(size := 10) %>%
+        scale_datetime('x',
+                       utc = FALSE,
+                       nice = 'second') %>%
+        set_options(width = 950, height = 350)
+      
+    })
+    
+    visplot %>% bind_shiny('plot1')
+    
+    #===================================plot2========================================================    
+    
+    output$plot2<-renderPlot({
+      
+      rawpath<-input$SOURCE
+      
+      if(is.null(rawpath)) {
+        return('Null')
+      }
+      
+      Tnod1<-as.POSIXct(input$Tnode1, '%Y-%m-%d %H:%M:%S', tz = 'UTC')
+      Tnod2<-as.POSIXct(input$Tnode2, '%Y-%m-%d %H:%M:%S', tz = 'UTC')
+      
+      raw<-fread(rawpath$datapath)[,Time:=as.POSIXct(Time, '%Y-%m-%d %H:%M:%S', tz = 'UTC')][,Speed:=Speed/3.6]
+      raw<-raw[Time >= Tnod1 & Time <= Tnod2]
+      
+      #=============================base data==============================================
+      
+      #定义bin
+      ##MOVES2014 bin定义編輯區(不包括Bin1)
+      Speedminnode<-c(-Inf,0.44704,0.44704,0.44704,0.44704,0.44704,0.44704,11.1760,11.1760,11.1760,11.1760,11.1760,11.1760,11.1760,11.1760,11.1760,22.3520,22.3520,22.3520,22.3520,22.3520,22.3520)
+      Speedmaxnode<-c(0.44704,11.1760,11.1760,11.1760,11.1760,11.1760,11.1760,22.3520,22.3520,22.3520,22.3520,22.3520,22.3520,22.3520,22.3520,22.3520,Inf,Inf,Inf,Inf,Inf,Inf)
+      VSPminnode<-c(-Inf,-Inf,0,3,6,9,12,-Inf,0,3,6,9,12,18,24,30,-Inf,6,12,18,24,30)
+      VSPmaxnode<-c(Inf,0,3,6,9,12,Inf,0,3,6,9,12,18,24,30,Inf,6,12,18,24,30,Inf)
+      Node<-data.table(VSPmin=VSPminnode,
+                       VSPmax=VSPmaxnode,
+                       vmin=Speedminnode,
+                       vmax=Speedmaxnode)
+      Node<-Node[,BIN:=as.character(paste('bin',2:23,sep = ''))]
+      Node<-as.data.frame(Node)
+      #end
+      
+      
+      #BIN匹配模板
+      Btable<-data.table(BIN=as.character(paste('bin',1:23, sep = '')),
+                         cnt=0)
+      #end
+      
+      #BIN加权平均函数
+      FUNweight<-function(x){weighted.mean(x,subraw$Fweight)}
+      
+      #end
+      
+      
+      #=============================base data end==============================================
+      
+      #=============================statistic argument=========================================
+      
+      #index input
+      
+      fueltype<-input$CarFuel#user setting
+      carweight<-input$CarGVW #unit:ton #user setting
+      wei.type<-ifelse(carweight<3.5, '1', ifelse(carweight>=3.5 & carweight<12, '2', 3))
+      reg.type<-input$CarReg#user setting
+      old.type<-ifelse(input$CarOdo<=79000, '1', ifelse(input$CarOdo>79000 & input$CarOdo<=161000, '2', ifelse(input$CarOdo>161000, '3', '1')))#defult
+      Id.sample<-paste0(fueltype,wei.type,reg.type,old.type)
+      
+      
+      #=============================select subsample end===============================================
+      
+      
+      #=============================bin colculate======================================================
+      
+      ##time diff calculate
+      raw<-raw[,Dtime:=c(0,difftime(Time[2:length(Time)],Time[1:(length(Time)-1)]) %>% as.numeric())]
+      
+      ##calculate acceleration(av1,av2,av3) and VSP
+      
+      raw<-raw[,av1:=list(c(0,diff(Speed)/as.numeric(difftime(Time[2:length(Time)],Time[1:(length(Time)-1)]))))]
+      raw<-raw[,c('av2','av3'):=list(c(0,av1[1:(length(av1)-1)]), c(0,0,av1[1:(length(av1)-2)]))]
+      raw<-raw[,c('h.delta','Vsum'):=list(c(0,diff(Altitude)),c(Speed+c(0,Speed[1:(length(Speed)-1)])))][,sinthita:=2*h.delta/(Vsum*Dtime)]
+      raw<-raw[,sinthita:=c(0,sinthita[2:length(sinthita)])][,sinthita:=ifelse(sinthita>=1,0,sinthita)][,!c('h.delta','Vsum')]
+      raw<-raw[,carweight:=carweight]
+      raw<-raw[,VSP:='Unknown']
+      raw<-within(raw,{
+        VSP[carweight<=3.855]<-(0.156461*Speed+0.00200193*Speed^2+0.000492646*Speed^3+carweight*Speed*(av1+9.81*sinthita))/1.4788#MOVES2009
+        VSP[3.855<carweight&carweight<=6.350]<-(0.0996*carweight*Speed+(0.00289+5.22*(10^-5)*carweight)*Speed^3+carweight*Speed*(av1+9.81*sinthita))#MOVES2014
+        VSP[6.350<carweight&carweight<=14.968]<-(0.0875*carweight*Speed+(0.00193+5.9*(10^-5)*carweight)*Speed^3+carweight*Speed*(av1+9.81*sinthita))#MOVES2014
+        VSP[14.968<carweight]<-(0.0661*carweight*Speed+(0.00289+4.21*(10^-5)*carweight)*Speed^3+carweight*Speed*(av1+9.81*sinthita))#MOVES2014
+      })
+      
+      raw<-raw[,VSP:=as.numeric(VSP)]
+      
+      #define BIN
+      raw<-raw[,BIN:='Unknown']
+      for(k in 1:length(Node$BIN)){
+        raw<-within(raw,{
+          BIN[VSP>=Node[k,1]&VSP<Node[k,2]&Speed>=Node[k,3]&Speed<Node[k,4]]<-Node[k,5]
+        })
+      }
+      
+      raw<-raw[,BIN:=ifelse(av1 <= -0.89408, 'bin1',
+                            ifelse( av1 < -0.44704 & av2 < -0.44704 & av3 < -0.44704, 'bin1', BIN))]
+      
+      raw<-raw[,c('Time','Speed','Altitude','BIN')]
+      
+      #=============================bin colculate end======================================================
+      
+      
+      
+      #=============================emission colculate=====================================================
+      
+      ##ef data
+      EF<-fread('F:/dengfanyuan/drivesoftware/www/mapping.csv', colClasses = c('character','character','character','numeric'))[,c('Index','BIN'):=list(paste0(Index,1), paste0('bin',Bin))][,!c('Bin')]
+      
+      ##set pollutant
+      
+      PT<-input$CarPt#user setting
+      
+      ##sub ef data
+      
+      subEF<-EF[Index == Id.sample & Species == PT][,!c('Species','Index')]
+      
+      
+      ##calculate BIN freq. in raw
+      
+      raw2<-raw[,.(Freq = .N), by = 'BIN'] %>% merge(Btable, by = 'BIN', all.y = TRUE)
+      raw2<-raw2[,!c('cnt')]
+      raw2[is.na(raw2)]<-0
+      raw2<-raw2[,Freq:=Freq/sum(Freq)]
+      
+      #calculate emission
+      
+      raw2<-merge(raw2,subEF, by = 'BIN', all.x = TRUE)[,emission:=ef*Freq*as.numeric(difftime(Tnod2,Tnod1, units = 'sec'))]
+      
+      TEM<-sum(raw2$emission)
+      
+      #=============================emission colculate end=====================================================
+      
+      #=============================plot=======================================================================
+      
+      raw2<-raw2[,BIN:=factor(BIN, levels = paste0('bin',c(1:23)))]
+      
+      p1<-ggplot(raw2, aes(x = BIN, y = emission))+
+        geom_bar(aes(fill = Freq*100),stat = 'identity', color = 'black')+
+        scale_x_discrete()+
+        scale_y_continuous(name = paste(PT,'emission (g)'))+
+        scale_fill_continuous(name = 'Time destribution (%)')+
+        labs(title = paste('Total',PT,'emission:',round(TEM,3),'g'),
+             subtitle = paste('Start:', Tnod1, '/', 'End:', Tnod2))+
+        theme(panel.background = element_rect(fill = NA, color = 'black', size = 1),
+              panel.border = element_rect(fill = NA, color = 'black', size = 1),
+              panel.grid = element_blank(),
+              axis.title.y = element_text(size = 20, face = 'bold', color = 'black', vjust = 3),
+              axis.title.x = element_blank(),
+              axis.text = element_text(size = 16, face = 'bold', color = 'black'),
+              legend.title = element_text(size = 20, face = 'bold', color = 'black'),
+              legend.text = element_text(size = 16, face = 'bold', color = 'black'),
+              legend.position = 'bottom',
+              legend.key.width = unit(3, units = 'cm'),
+              plot.title = element_text(size = 24, face = 'bold', color = 'black'),
+              plot.subtitle = element_text(size = 22, face = 'bold.italic', color = 'gray30'))
+      
+      print(p1)
+      
+    })
+    
+    
   })
-}
+  
+  
+})
--- ui.R
+++ ui.R
@@ -1,16 +1,167 @@
-# Define UI for application that draws a histogram
-dashboardPage(
-  dashboardHeader(title = "课题组薪资收入分布"),
-  dashboardSidebar(),
+ui<-dashboardPage(
+  
+  dashboardHeader(title = strong('机动车动态排放模型'),
+                  titleWidth = 300),
+  dashboardSidebar(width = 300,
+                   sidebarMenu(
+                     menuItem(strong("首页"), tabName = 'homepage', selected = TRUE, icon = icon('home')),
+                     menuItem(strong('文件/参数输入'), tabName = 'FAinput', icon = icon('gear')),
+                     menuItem(strong('分析结果输出'), tabName = 'PDoutput', icon = icon('bar-chart-o'))
+                   )),
   dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
+    tags$p(
+      tags$style(HTML("
+                      
+                      p {
+                      font-family: '微软雅黑';
+                      font-weight: 500;
+                      line-height: 1.1;
+                      color: #fff;
+                      }
+                      
+                      "))
+      ),
+    
+    tags$strong(
+      tags$style(HTML("
+                      
+                      strong {
+                      font-family: '微软雅黑';
+                      font-weight: bold;
+                      line-height: 1.1;
+                      }
+                      
+                      "))
+      ),
+    
+    tabItems(
+      tabItem(tabName = 'homepage',
+              fluidPage(
+                box(
+                  title = strong('欢迎使用'),
+                  width = 200,
+                  p("p creates a paragraph of text."),
+                  p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+                  strong("strong() makes bold text."),
+                  em("em() creates italicized (i.e, emphasized) text."),
+                  br(),
+                  code("code displays your text similar to computer code"),
+                  div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+                  br(),
+                  p("span does the same thing as div, but it works with",
+                    span("groups of words", style = "color:blue"),
+                    "that appear inside a paragraph.")
+                )
+              )
+      ),
       
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      tabItem(tabName = 'FAinput',
+              fluidPage(
+                box(
+                  title = strong('视图'),
+                  width = 300,
+                  height = 400,
+                  ggvisOutput('plot1')
+                ),
+                box(
+                  title = strong('创建输入数据'),
+                  column(width = 3,
+                         box(
+                           title = strong('上传数据源'),
+                           fileInput('SOURCE', label = '格式要求: csv 文件',
+                                     width = 300),
+                           width = 100,
+                           status = 'primary',
+                           solidHeader = TRUE
+                         )),
+                  column(width = 9,
+                         tabBox(
+                           side = 'left', height = '250px',
+                           tabPanel(title = strong('数据总览'),
+                                    
+                                    sliderInput('Trange', label = '拖动时间条',
+                                                min = as.POSIXct('2016-01-01 01:00:00', tz = 'UTC'), 
+                                                max = as.POSIXct('2018-08-01 23:00:00', tz = 'UTC'), 
+                                                value = as.POSIXct(c('2016-01-01 09:00:00', '2016-01-01 10:00:00'), '%Y-%m-%d %H:%M:%S', tz = 'UTC'), 
+                                                width = 600,
+                                                timeFormat = '%Y-%m-%d %H:%M:%S',
+                                                timezone = '+0000',
+                                                step = 1)
+                                    
+                           ),
+                           tabPanel(title = strong('时间段设置'),
+                                    column(
+                                      width = 6,
+                                      box(
+                                        title = '1. 开始',
+                                        textInput('Tnode1', label = '示例: 2016-01-01 00:00:01'),
+                                        width = 100
+                                      )
+                                    ),
+                                    column(
+                                      width = 6,
+                                      box(
+                                        title = '2.结束',
+                                        textInput('Tnode2', label = '示例: 2016-01-01 00:00:01'),
+                                        width = 100
+                                      )
+                                    )
+                                    
+                           ),
+                           tabPanel(title = strong('输入车辆信息'),
+                                    box(
+                                      title = '燃料类型',
+                                      selectInput(inputId = 'CarFuel', label = '其他燃料包括天然气、液化石油气等', 
+                                                  choices = c('柴油' = '1',
+                                                              '汽油' = '2',
+                                                              '其他燃料' = '3')),
+                                      width = 3
+                                    ),
+                                    box(
+                                      title = '车辆总质量(吨)',
+                                      numericInput(inputId = 'CarGVW', label = '可手动输入或通过箭头控件选择',
+                                                   value = 1.5, min = 0, max = 200),
+                                      width = 3
+                                    ),
+                                    box(
+                                      title = '排放标准',
+                                      selectInput(inputId = 'CarReg', label = '请根据车辆合格证信息选择', 
+                                                  choices = c('国一前' = '0',
+                                                              '国一' = '1',
+                                                              '国二' = '2',
+                                                              '国三' = '3',
+                                                              '国四' = '4',
+                                                              '国五' = '5',
+                                                              '国六' = '6')),
+                                      width = 3
+                                    ),
+                                    box(
+                                      title = '里程表读数(km)',
+                                      numericInput(inputId = 'CarOdo', label = '可手动输入或通过箭头控件选择',
+                                                   value = 40000, min = 0, max = 20000000),
+                                      width = 3
+                                    )),
+                           width = 200
+                         )),
+                  width = 300,
+                  height = 300
+                )
+              )
+      ),
+      
+      tabItem(
+        
+        tabName = 'PDoutput',
+        fluidPage(
+          column(width = 2,
+                 selectInput('CarPt','污染物',
+                             c('CO','HC','NOx','PM2.5'))),
+          plotOutput('plot2', height = 350)
+        )
+        
       )
+      
     )
-  )
-)
+      ),
+  skin = 'purple'
+      )
